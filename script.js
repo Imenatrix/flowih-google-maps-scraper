0624ae532d2629ac33ebf31a352cb83afb08d354
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-12 19:16:11 -0300

    Corrige inicialização sem a modal

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 var modal = document.querySelector('.w6VYqd').querySelectorAll('.aIFcqe')[1]
+var modal_button = document.querySelector('svg')?.parentElement.parentElement
 var cards = document.querySelectorAll('[role="article"]')
 var list = document.querySelector('[role="feed"]')
 var obs = new MutationObserver(callback)
-var counter = 0
+var counter = 0;
 
-cards[counter].firstChild.click()
+(modal_button || cards[1].firstChild).click()
 obs.observe(modal, {childList : true})
 
 var output = []
 
 function callback() {
-    var name = document.querySelector('.DUwDvf')?.textContent
-    var phone = document.querySelector('[data-item-id^="phone:"]')?.ariaLabel.split(': ')[1]
-    var adress = document.querySelector('[data-item-id="address"]')?.ariaLabel.split(': ')[1]
-    output.push([name, phone, `"${adress}"`])
-    counter++
     if (counter == cards.length) {
         chrome.runtime.sendMessage({type : 'completed', payload : output})
         obs.disconnect()
@@ -22,4 +18,15 @@ function callback() {
     else {
         cards[counter].firstChild.click()
     }
+
+    if (counter > 0) {
+        var name = document.querySelector('.DUwDvf')?.textContent
+        var phone = document.querySelector('[data-item-id^="phone:"]')?.ariaLabel.split(': ')[1]
+        var adress = document.querySelector('[data-item-id="address"]')?.ariaLabel.split(': ')[1]
+        var line = [name, phone, `"${adress}"`]
+        console.log(line)
+        output.push(line)
+    }
+
+    counter++
 }
