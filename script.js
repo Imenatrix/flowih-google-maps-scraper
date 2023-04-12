bd0780f846b7b1d6e4b43b7df657d1869a681b06
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-12 09:00:58 -0300

    Implementa loop de scraping

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var modal = document.querySelectorAll('[role="main"')[1].parentElement
+var cards = document.querySelectorAll('[role="article"')
+var counter = 0
+
+cards[0].firstChild.click()
+
 console.log(modal)
 var obs = new MutationObserver(() => {
     var name = document.querySelector('.DUwDvf')?.textContent
@@ -7,5 +12,7 @@ var obs = new MutationObserver(() => {
     console.log(name)
     console.log(phone)
     console.log(adress)
+    counter ++
+    cards[counter].firstChild.click()
 })
 obs.observe(modal, {childList : true})
