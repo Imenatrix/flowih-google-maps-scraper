75bd084416642d3bd522c9d6402f24f7b38ab695
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-04-12 15:35:34 -0300

    Implementa download do CSV

--- popup.js
+++ popup.js
@@ -1,8 +1,19 @@
 var btnCarregar = document.getElementById('btn-carregar')
 
+function downloadBlob(content, filename, contentType) {
+    // Create a blob
+    var blob = new Blob([content], { type: contentType });
+    var url = URL.createObjectURL(blob);
+  
+    // Create a link to download it
+    var pom = document.createElement('a');
+    pom.href = url;
+    pom.setAttribute('download', filename);
+    pom.click();
+}
+
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
 }
@@ -16,7 +27,39 @@ async function carregar() {
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message)
+    const output = convertArrayToCSV(message.payload);
+    const blob = new Blob([output], {type : 'text/csv;charset=utf-8;'})
+    const url = URL.createObjectURL(blob)
+
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', 'export.csv')
+    link.innerText = 'Download'
+
+    document.body.appendChild(link)
 })
 
 btnCarregar.addEventListener('click', carregar)
+
+function convertArrayToCSV(array) {
+    let output = '';
+    for (let i = 0; i < array.length; i++) {
+        const row = array[i];
+
+        let line = '';
+        for (let j = 0; j < row.length; j++) {
+            const cell = row[j];
+            if (cell) {
+                line += cell.trim();
+            }
+            if (j != row.length - 1) {
+                line += ',';
+            }
+        }
+        if (i != array.length - 1) {
+            line += '\n';
+        }
+        output += line;
+    }
+    return output;
+}
